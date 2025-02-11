eedc25146ac8451a3b46c0b0fdf61b854a086d46
Marcelo Nobre <marcnobremorais@gmail.com>
2025-02-11 22:55:45 +0000

    Add query and update endpoint to save user data (test only)

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from lib.lib_db_manager import DBManager
 
 app = Flask(__name__)
+db_manager = DBManager()  # Criando uma instância da classe DBManager
 
 @app.route('/')
 def home():
-    return "Hello World!"
+	return "Hello World!"
+
+@app.route('/users', methods=['GET'])
+def get_users():
+	users = db_manager.get_all_users()  # Usando o método para obter todos os usuários
+	return jsonify(users)
+
+@app.route('/users', methods=['POST'])
+def create_user():
+	name = request.json.get('name')
+	email = request.json.get('email')
+	db_manager.create_user(name, email)  # Usando o método para criar um usuário
+	return jsonify({"message": "User created successfully!"}), 201
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+	app.run(host='0.0.0.0', port=5000)
