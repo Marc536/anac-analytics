9404da800e6824526c73eb823359844bb398902f
Marcelo Nobre <marcnobremorais@gmail.com>
2025-02-12 16:08:09 +0000

    Prevent db_manager object from being created multiple times

--- app.py
+++ app.py
@@ -4,20 +4,27 @@ from lib.lib_db_manager import DBManager
 app = Flask(__name__)
 db_manager = DBManager()  # Criando uma instância da classe DBManager
 
+def get_db_manager():
+	global db_manager
+	if db_manager is None:
+		db_manager = DBManager()
+	
+	return db_manager
+
 @app.route('/')
 def home():
 	return "Hello World!"
 
 @app.route('/users', methods=['GET'])
 def get_users():
-	users = db_manager.get_all_users()  # Usando o método para obter todos os usuários
+	users = get_db_manager().get_all_users()  # Usando o método para obter todos os usuários
 	return jsonify(users)
 
 @app.route('/users', methods=['POST'])
 def create_user():
 	name = request.json.get('name')
 	email = request.json.get('email')
-	db_manager.create_user(name, email)  # Usando o método para criar um usuário
+	get_db_manager().create_user(name, email)  # Usando o método para criar um usuário
 	return jsonify({"message": "User created successfully!"}), 201
 
 if __name__ == '__main__':
