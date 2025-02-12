e5bb6addd538a6ec7991f1ea8e5dfc517d7388f0
Marcelo Nobre <marcnobremorais@gmail.com>
2025-02-12 20:46:00 +0000

    Add post_anac_statistical_data

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import logging
+import pandas as pd
+
 from flask import Flask, jsonify, request
 from lib.lib_db_manager import DBManager
 
@@ -20,12 +23,18 @@ def get_users():
 	users = get_db_manager().get_all_users()  # Usando o método para obter todos os usuários
 	return jsonify(users)
 
-@app.route('/users', methods=['POST'])
-def create_user():
-	name = request.json.get('name')
-	email = request.json.get('email')
-	get_db_manager().create_user(name, email)  # Usando o método para criar um usuário
-	return jsonify({"message": "User created successfully!"}), 201
+@app.route('/post_anac_statistical', methods=['POST'])
+def post_anac_statistical_data():
+	try:
+		file = request.files['file']
+		df = pd.read_csv(file.stream, delimiter=';', skiprows=1)
+		get_db_manager().post_table_anac(df)
+		return jsonify({"message": "CSV uploaded and saved to PostgreSQL successfully!"}), 201
+	except Exception as e:
+		logging.error(f"{e}")
+		return jsonify({"message": "Error uploading the CSV file. \
+			Please check if the file is in the correct format and try again."
+		}), 400
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
